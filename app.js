54cfb63f479db196bfdd031d53047aeb47b4fe7b
PJ <peterjoe2005@gmail.com>
2024-07-08 21:39:20 -0400

    Hosting on heroku

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
