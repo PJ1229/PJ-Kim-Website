229919beba7dde5a1d0bec93a749b1c5ecf44dd1
PJ <peterjoe2005@gmail.com>
2024-07-12 21:33:08 -0400

    cleaned code and changed format

--- public/desktopcheck.js
+++ public/desktopcheck.js
@@ -1,30 +1,21 @@
-/* Storing user's device details in a variable*/
-let details = navigator.userAgent; 
-  
-/* Creating a regular expression  
-containing some mobile devices keywords  
-to search it in details string*/
-let regexp = /android|iphone|kindle|ipad/i; 
-  
-/* Using test() method to search regexp in details 
-it returns boolean value*/
-let isMobileDevice = regexp.test(details); 
+let details = navigator.userAgent;
+let regexp = /android|iphone|kindle|ipad/i;
+let isMobileDevice = regexp.test(details);
 const container = document.querySelectorAll('button.container');
 const mobile = document.querySelectorAll('button.mobile');
-  
-if (isMobileDevice) { 
-    container.forEach((container) => {
+
+if (isMobileDevice) {
+    container.forEach(container => {
         container.style.visibility = "hidden";
     });
-    mobile.forEach((mobile) => {
+    mobile.forEach(mobile => {
         mobile.style.visibility = "visible";
     });
-   
-} else { 
-    container.forEach((container) => {
+} else {
+    container.forEach(container => {
         container.style.visibility = "visible";
     });
-    mobile.forEach((mobile) => {
+    mobile.forEach(mobile => {
         mobile.style.visibility = "hidden";
     });
 }
--- public/observer.js
+++ public/observer.js
@@ -1,25 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const title = document.querySelector('.title');
-
-    // Define your opacity multiplier here
-    const opacityMultiplier = 0.75; // Change this value to adjust the fade rate (1 is a full range from 1 to 0)
-    const translationMultiplier = -.5; // Adjust this for background position movement
+    const opacityMultiplier = 0.75;
+    const translationMultiplier = -0.5;
 
     window.addEventListener('scroll', () => {
         const scrollTop = window.scrollY;
         const windowHeight = window.innerHeight;
-
-        // Calculate the scroll position relative to the viewport height
         const clampedScrollPosition = Math.max(0, Math.min(1, scrollTop / windowHeight));
-
-        // Adjust opacity based on scroll position for the top section
         const opacity = 1 - (clampedScrollPosition * opacityMultiplier);
         title.style.opacity = opacity;
-
-        // Debug log to check opacity value
-        //console.log('Opacity:', opacity);
-
-        // Adjust background position based on scroll for the top section
         const translateY = scrollTop * translationMultiplier;
         title.style.backgroundPositionY = `-${translateY}px`;
     });
@@ -27,34 +16,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelector('.buttons');
-
-    // Define your translation multiplier here
-    const translationMultiplier = -1.75; // Change this value to adjust the speed of the scrolling effect
+    const translationMultiplier = -1.75;
 
     window.addEventListener('scroll', () => {
         const scrollTop = window.scrollY;
-
-        // Calculate the translation based on scroll position and the translation multiplier
         const translateY = scrollTop * translationMultiplier; 
-
-        // Apply the translation using transform property
         if (buttons) {
             buttons.style.transform = `translateY(${translateY}px)`;
         }
     });
 });
 
-
 document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
-                console.log(entry);  // Log each entry
                 if (entry.isIntersecting) {
                     entry.target.classList.add("in-view-arrow");
                     entry.target.classList.remove("out-view-arrow");
-                }
-                else{
+                } else {
                     entry.target.classList.remove("in-view-arrow");
                     entry.target.classList.add("out-view-arrow");
                 }
@@ -67,46 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
     );
 
     const tags = document.querySelectorAll('figure');
-    console.log(tags);  // Log the NodeList of figure elements
-
-    tags.forEach((tag) => {
-        observer.observe(tag);
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                console.log(entry);  // Log each entry
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("in-view-img");
-                    entry.target.classList.remove("out-view-img");
-                    const overlay = entry.target.parentNode.querySelector('.overlay');
-                    if (overlay) {
-                        overlay.classList.add("in-view-overlay");
-                        overlay.classList.remove("out-view-overlay");
-                    }
-                } else {
-                    entry.target.classList.remove("in-view-img");
-                    entry.target.classList.add("out-view-img");
-                    const overlay = entry.target.parentNode.querySelector('.overlay');
-                    if (overlay) {
-                        overlay.classList.remove("in-view-overlay");
-                        overlay.classList.add("out-view-overlay");
-                    }
-                }
-            });
-        }, 
-        {
-            rootMargin: "0px",
-            threshold: [0, 0.1, 1]
-        }
-    );
-
-    const tags = document.querySelectorAll('button.container');
-    console.log(tags);  // Log the NodeList of drop-down images
-
     tags.forEach((tag) => {
         observer.observe(tag);
     });
