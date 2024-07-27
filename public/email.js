18def8c5cbd295638f5328e05912dda369ae866d
PJ <peterjoe2005@gmail.com>
2024-07-27 14:16:23 -0400

    added a difference in mobile and desktop

--- public/email.js
+++ public/email.js
@@ -15,7 +15,7 @@ function handleSubmit(event) {
     })
     .then(response => response.text())
     .then(data => {
-        location.replace("newsletter.html");
+        location.replace("index.html");
     })
     .catch(error => {
         console.error('Error:', error);
--- public/mobile.js
+++ public/mobile.js
@@ -1,19 +1,19 @@
 let details = navigator.userAgent;
 let regexp = /android|iphone|kindle|ipad/i;
 let isMobileDevice = regexp.test(details);
-const container = document.querySelectorAll('button.container');
-const mobile = document.querySelectorAll('button.mobile');
+const desktop = document.querySelectorAll('.desktop');
+const mobile = document.querySelectorAll('.mobile');
 
 if (isMobileDevice) {
-    container.forEach(container => {
-        container.style.visibility = "hidden";
+    desktop.forEach(desktop => {
+        desktop.style.visibility = "hidden";
     });
     mobile.forEach(mobile => {
         mobile.style.visibility = "visible";
     });
 } else {
-    container.forEach(container => {
-        container.style.visibility = "visible";
+    desktop.forEach(desktop => {
+        desktop.style.visibility = "visible";
     });
     mobile.forEach(mobile => {
         mobile.style.visibility = "hidden";
