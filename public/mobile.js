816442ba1a45b5d9758c67f2ddd0fad05491128a
PJ <peterjoe2005@gmail.com>
2024-07-28 11:50:28 -0400

    changed button sizing on mobile vs desktop

--- public/mobile.js
+++ public/mobile.js
@@ -1,21 +1,30 @@
 let details = navigator.userAgent;
 let regexp = /android|iphone|kindle|ipad/i;
 let isMobileDevice = regexp.test(details);
+let isPortrait = window.matchMedia("(orientation: portrait)").matches;
 const desktop = document.querySelectorAll('.desktop');
 const mobile = document.querySelectorAll('.mobile');
 
-if (isMobileDevice) {
-    desktop.forEach(desktop => {
-        desktop.style.visibility = "hidden";
-    });
-    mobile.forEach(mobile => {
-        mobile.style.visibility = "visible";
-    });
-} else {
-    desktop.forEach(desktop => {
-        desktop.style.visibility = "visible";
-    });
-    mobile.forEach(mobile => {
-        mobile.style.visibility = "hidden";
-    });
+function updateVisibility() {
+    if (isMobileDevice && isPortrait) {
+        desktop.forEach(desktop => {
+            desktop.style.visibility = "hidden";
+        });
+        mobile.forEach(mobile => {
+            mobile.style.visibility = "visible";
+        });
+    } else {
+        desktop.forEach(desktop => {
+            desktop.style.visibility = "visible";
+        });
+        mobile.forEach(mobile => {
+            mobile.style.visibility = "hidden";
+        });
+    }
 }
+
+updateVisibility();
+
+window.addEventListener("orientationchange", () => {
+    location.reload();
+});
