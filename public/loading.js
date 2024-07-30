390d32c51a76b0c5bd74d096b19ec15eb7db3729
PJ <peterjoe2005@gmail.com>
2024-07-30 09:39:53 -0400

    fixed scrolling bug

--- public/loading.js
+++ public/loading.js
@@ -2,11 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const loadingScreen = document.getElementById("loading-screen");
     const content = document.getElementById("content");
 
+    // Add no-scroll class to body
+    document.body.classList.add('no-scroll');
+
     window.addEventListener("load", function() {
-        // Ensure a minimum loading time to improve user experience
-        setTimeout(() => {
-            loadingScreen.style.display = "none";
-            content.style.display = "block";
-        }, 0); // Adjust this value to set a minimum delay, e.g., 1000ms (1 second)
+      // Ensure a minimum loading time to improve user experience
+      setTimeout(() => {
+        loadingScreen.style.display = "none";
+        content.style.display = "block";
+        // Remove no-scroll class from body
+        document.body.classList.remove('no-scroll');
+      }, 3000); // Adjust this value to set a minimum delay, e.g., 1000ms (1 second)
     });
-});
+  });
